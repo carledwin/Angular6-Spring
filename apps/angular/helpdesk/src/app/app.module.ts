91337105947eb11c8e66c151e722388636df7d90
Carl Edwin <carlinstr@gmail.com>
2018-09-09 01:24:38 -0300

    Desenvolvimento do login concluido

--- apps/angular/helpdesk/src/app/app.module.ts
+++ apps/angular/helpdesk/src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { AuthGuard } from './components/security/auth.guard';
 import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SharedService } from './services/shared.service';
+import { UserService } from './services/user.service';
+import { routes } from './api.routes';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './components/security/auth.interceptor';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/security/login/login.component';
-import { routes } from './api.routes';
-import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,12 @@ import { UserService } from './services/user.service';
   providers: [
     UserService,
     SharedService,
-   
+    AuthGuard,
+   {
+     provide: HTTP_INTERCEPTORS,
+     useClass: AuthInterceptor,
+     multi: true
+   }
   ],
   bootstrap: [AppComponent]
 })
